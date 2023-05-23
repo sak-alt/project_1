c3756139464df27c37fca72d049978bc9a07eacc
codetalks95 <info@codetalksdna.com>
2023-05-23 09:17:56 +0530

    Added new Class

---
+++ src/GeneralCoreJavaProgram/Test.java
@@ -0,0 +1,27 @@
+package GeneralCoreJavaProgram;
+
+public class Test {
+    public static void main(String[] args) {
+        int numberOne = 10;
+        int numberTwo = 20;
+        String nameValue = "Sakhi";
+        System.out.println("welcome to java");
+        System.out.println(numberTwo + numberOne);
+        System.out.println(nameValue);
+        additionOfTwoNumber(numberOne, numberTwo);
+        subtractionOfTwoNumber(numberOne, numberTwo);
+    }
+
+    public static void additionOfTwoNumber(int numberOne, int numberTwo) {
+        int sum;
+        sum = numberOne + numberTwo;
+        System.out.println(sum);
+    }
+
+
+    public static void subtractionOfTwoNumber(int numberOne, int numberTwo) {
+        int sub;
+        sub = numberOne - numberTwo;
+        System.out.println(sub);
+    }
+}
