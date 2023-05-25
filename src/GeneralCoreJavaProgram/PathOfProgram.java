ebdc682e6573262f9a71bc91a2e537e3c2a98682
codetalks95 <info@codetalksdna.com>
2023-05-25 11:37:57 +0530

    Added new Logics and Classes.

--- src/GeneralCoreJavaProgram/PathOfProgram.java
+++ src/GeneralCoreJavaProgram/PathOfProgram.java
@@ -1,8 +1,8 @@
 package GeneralCoreJavaProgram;
 
 public class PathOfProgram {
-    static String path = "C:\\Users\\Sakhi Chaudhary\\IdeaProjects\\project_1";
-//    static String path = "D:\\Intellij-Idea-Projects\\project_1";
+//    static String path = "C:\\Users\\Sakhi Chaudhary\\IdeaProjects\\project_1";
+    static String path = "D:\\Intellij-Idea-Projects\\project_1";
 
     public static String getPathOfPrograms() {
         return path;
