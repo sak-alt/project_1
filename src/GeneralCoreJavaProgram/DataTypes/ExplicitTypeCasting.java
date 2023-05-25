f526cc1a758c2f09c9e9b8b3d2493066901436eb
codetalks95 <info@codetalksdna.com>
2023-05-25 09:29:13 +0530

    Added new Logics and Classes.

--- src/GeneralCoreJavaProgram/DataTypes/ExplicitTypeCasting.java
+++ src/GeneralCoreJavaProgram/DataTypes/ExplicitTypeCasting.java
@@ -2,6 +2,16 @@ package GeneralCoreJavaProgram.DataTypes;
 
 public class ExplicitTypeCasting {
     public static void main(String[] args) {
+        byte number = (byte) 220;
+        System.out.println(number);
 
+        short number1 = (short) 32768;
+        System.out.println(number1);
+
+        int number3 =(int) 20.44;
+        System.out.println(number3);
+
+        long number4 =(long) 20.44;
+        System.out.println(number4);
     }
 }
