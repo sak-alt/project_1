633afaf09441b9f3c82791064db1281c24edabba
codetalks95 <info@codetalksdna.com>
2023-06-02 13:51:35 +0530

    Added Code For Encapsulation.

--- src/GeneralCoreJavaProgram/EncapsulationExample/EncapsulationExample.java
+++ src/GeneralCoreJavaProgram/EncapsulationExample/EncapsulationExample.java
@@ -2,7 +2,6 @@ package GeneralCoreJavaProgram.EncapsulationExample;
 
 public class EncapsulationExample {
     private int bankBalance;
-    private int bankAccount;
 
     public int getBankBalance() {
         return bankBalance;
@@ -12,11 +11,21 @@ public class EncapsulationExample {
         this.bankBalance = bankBalance;
     }
 
-    public int getBankAccount() {
-        return bankAccount;
+    public void deposit(double amount) {
+        if (amount > 0) {
+            bankBalance += amount;
+            System.out.println("Deposit successful. New balance: " + bankBalance);
+        } else {
+            System.out.println("Invalid amount for deposit.");
+        }
     }
 
-    public void setBankAccount(int bankAccount) {
-        this.bankAccount = bankAccount;
+    public void withdraw(double amount) {
+        if (amount > 0 && bankBalance >= amount) {
+            bankBalance -= amount;
+            System.out.println("Withdrawal successful. New balance: " + bankBalance);
+        } else {
+            System.out.println("Insufficient balance or invalid amount for withdrawal.");
+        }
     }
 }
--- src/GeneralCoreJavaProgram/EncapsulationExample/EncapsulationMain.java
+++ src/GeneralCoreJavaProgram/EncapsulationExample/EncapsulationMain.java
@@ -1,20 +1,10 @@
 package GeneralCoreJavaProgram.EncapsulationExample;
 
-import java.util.Scanner;
-
 public class EncapsulationMain {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
         EncapsulationExample encapsulationExample = new EncapsulationExample();
-        encapsulationExample.setBankAccount(123456);
-        encapsulationExample.setBankBalance(20000000);
-
-        System.out.println("Enter the Bank Account Number");
-        int bankAccountNumberEnteredByUser = scanner.nextInt();
-        if (encapsulationExample.getBankAccount() == bankAccountNumberEnteredByUser) {
-            System.out.println(encapsulationExample.getBankBalance());
-        } else {
-            System.out.println("You are not the Valid Personnel");
-        }
+        encapsulationExample.setBankBalance(100);
+        encapsulationExample.deposit(200);
+        encapsulationExample.withdraw(300);
     }
 }
