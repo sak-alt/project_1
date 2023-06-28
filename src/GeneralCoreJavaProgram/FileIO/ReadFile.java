f3e0bff1656a0775c2bdf4f84795e7ac84d72c12
codetalks95 <info@codetalksdna.com>
2023-06-28 10:09:41 +0530

    Fixed Code with the line Issues.

--- src/GeneralCoreJavaProgram/FileIO/ReadFile.java
+++ src/GeneralCoreJavaProgram/FileIO/ReadFile.java
@@ -6,11 +6,8 @@ public class ReadFile {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new FileReader("C:\\Users\\91830\\Downloads\\xyz\\codetalksdna.txt"));
         String line;
-        while (bufferedReader.readLine()!=null){
-            System.out.println(bufferedReader.readLine());
-        }
-        for (int i = 0; i < 4; i++) {
-            System.out.println(bufferedReader.readLine());
+        while ((line = bufferedReader.readLine()) != null) {
+            System.out.println("The Data in the files are: " + line);
         }
         bufferedReader.close();
     }
